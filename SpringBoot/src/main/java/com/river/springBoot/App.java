db117a88464e6f16eebdf4cf80996c52474a3c59
robertchiang0520 <robertchiang0520@gmail.com>
2016-05-19 11:39:24 +0800

    springboot

--- SpringBoot/src/main/java/com/river/springBoot/App.java
+++ SpringBoot/src/main/java/com/river/springBoot/App.java
@@ -1,13 +1,19 @@
 package com.river.springBoot;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class App {
+    public static void main( String[] args ) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+    
+    
+    @RequestMapping("/") public String home() {
+    	return "hello world";
     }
 }
