1080f039c9555eec0b03e24185a0b90b38ea7088
robertchiang0520 <robertchiang0520@gmail.com>
2016-05-19 14:24:49 +0800

    initial with h2

--- SpringBoot/src/main/java/com/river/springBoot/web/HomeController.java
+++ SpringBoot/src/main/java/com/river/springBoot/web/HomeController.java
@@ -1,10 +1,9 @@
 package com.river.springBoot.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-// @Controller
-@RestController
+@Controller
 public class HomeController {
 
     @RequestMapping("/home") public String home() {
