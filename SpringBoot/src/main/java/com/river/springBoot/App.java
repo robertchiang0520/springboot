d90d1a283ddadf8676a9d7d3ed842ad66fd4b0d2
robertchiang0520 <robertchiang0520@gmail.com>
2016-05-19 11:50:28 +0800

    init

--- SpringBoot/src/main/java/com/river/springBoot/App.java
+++ SpringBoot/src/main/java/com/river/springBoot/App.java
@@ -2,13 +2,16 @@ package com.river.springBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
+@ComponentScan
 public class App {
     public static void main( String[] args ) throws Exception {
+    	System.setProperty("spring.devtools.restart.enabled", "true"); // enable restart
         SpringApplication.run(App.class, args);
     }
     
