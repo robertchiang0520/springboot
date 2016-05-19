db25ce7f5eed95808053dde2787a689733c9b3c7
robertchiang0520 <robertchiang0520@gmail.com>
2016-05-19 14:45:15 +0800

    web mvc

--- SpringBoot/src/main/java/com/river/springBoot/SecurityConfiguration.java
+++ SpringBoot/src/main/java/com/river/springBoot/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package com.river.springBoot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,12 +10,31 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests()
-        	.antMatchers("/").permitAll()
-        	.and().authorizeRequests().antMatchers("/console/**").permitAll();
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        	.authorizeRequests()
+    			.antMatchers("/console/**").permitAll()
+    			.and()
+        	.authorizeRequests()
+        		.antMatchers("/").permitAll().anyRequest().authenticated()
+        		.and()
 
-        httpSecurity.csrf().disable();
-        httpSecurity.headers().frameOptions().disable();
+        	.formLogin()
+            	.loginPage("/login")
+            	.permitAll()
+            	.and()
+            .logout()
+                .permitAll()
+        	;
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
     }
 }
